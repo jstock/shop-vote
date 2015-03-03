92fc85ede0e3220c3f3281449e6c03f8467df706
warsame20 <ibrahim.71@osu.edu>
2015-03-02 16:52:41 -0800

    created a filter profile model

---
+++ ShopVote/Models/FilterProfileModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.collections.generic;
+using System.componentModel.DataAnnotations;
+using System.componentModel.DataAnnonnotaions.schema;
+using System.data.Entity;
+using System.Linq;
+using System.web;
+
+namespace ShopVote.Models
+{
+     public class FilterProfilesContext : DbContext
+     {
+        public FilterProfilesContext()
+         :  base("DefaultConnection")
+        {
+        }
+        public DbSet<Filter> FilterProfiles { get;set; }
+    }
+
+    [Table("FilterProfiles")]
+    public class FilterProfile
+    {
+     [key]
+     [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+     public int Id  {get;set;}
+     [ForeigKey("User")]
+     public int UserId{get;set;}
+     [ForeigKey("Filter")]
+     public int FilterId{get;set;}
+     [ForeigKey("FilterCategory")]
+     public int CategoryId{get;set;}
+     public Object Value {get;set;}
+    }
+}
