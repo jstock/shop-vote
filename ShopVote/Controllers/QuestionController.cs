fc8fa1e9d2190f9f9cfa93649c31a7ce4f343e85
feysal <ibrahim.71@osu.edu>
2015-03-08 00:46:47 -0800

    Create QuestionController.cs

---
+++ ShopVote/Controllers/QuestionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using ShopVote.Filterprofiles;
+using ShopVote.Models;
+
+namespace ShopVote.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            //code for the saving question data will go here
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
