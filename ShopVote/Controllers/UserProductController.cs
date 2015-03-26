ccbfbf7c3d10fb4515875ff3155224b8c77dcfa1
ESchanz10 <schanz.10@buckeyemail.osu.edu>
2015-03-26 16:04:24 -0400

    New UserProductViews

--- ShopVote/Controllers/UserProductController.cs
+++ ShopVote/Controllers/UserProductController.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopVote.Models;
 
 namespace ShopVote.Controllers
 {
     public class UserProductController : Controller
     {
+        private ProductContext db = new ProductContext();
         //
         // GET: /UserProduct/
 
         public ActionResult Index()
+        {
+            var Products = db.Products.Where(p => p.Id > 0);
+            return View(Products.ToList());
+        }
+        public ActionResult Details(int id = 0)
         {
             return View();
         }
