3d3d7625f5a4777fa7a0d68e3c8d3000d5c5e087
ESchanz10 <schanz.10@buckeyemail.osu.edu>
2015-03-26 15:25:01 -0400

    New Controller

---
+++ ShopVote/Controllers/UserProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopVote.Controllers
+{
+    public class UserProductController : Controller
+    {
+        //
+        // GET: /UserProduct/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
