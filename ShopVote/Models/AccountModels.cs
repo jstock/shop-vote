caf02261dcbdb48fa14d72567cdbc22ec41f4c24
jstock <jstock3293@gmail.com>
2015-04-01 11:10:20 -0400

    added sliding login box to Shared/Layout

--- ShopVote/Models/AccountModels.cs
+++ ShopVote/Models/AccountModels.cs
@@ -58,15 +58,15 @@ namespace ShopVote.Models
   public class LoginModel
   {
     [Required]
-    [Display(Name = "User name")]
+    [Display(Name = "username")]
     public string UserName { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
-    [Display(Name = "Password")]
+    [Display(Name = "password")]
     public string Password { get; set; }
 
-    [Display(Name = "Remember me?")]
+    [Display(Name = "remember me?")]
     public bool RememberMe { get; set; }
   }
 
