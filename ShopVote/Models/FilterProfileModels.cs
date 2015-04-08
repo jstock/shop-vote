9b8280f43618b2c7a2675370134bf43e8fd165b8
jstock <jstock3293@gmail.com>
2015-04-08 13:28:05 -0400

    updated admin/products paging, created FilterProfile model

---
+++ ShopVote/Models/FilterProfileModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ShopVote.Models
+{
+  public class FilterProfilesContext : DbContext {
+
+      public FilterProfilesContext() :
+        base("DefaultConnection") {
+        }
+        public DbSet<FilterProfile> FilterProfiles { get; set; }
+  }
+
+  [Table("FilterProfiles")]
+  public class FilterProfile {
+    [Key]
+    [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+    public int Id;
+
+    public virtual int UserID { get; set; }
+    [ForeignKey("UserID")]
+    public virtual UserProfile User { get; set; }
+
+    public virtual int FilterID { get; set; }
+    [ForeignKey("FilterID")]
+    public virtual PMFilter Filter { get; set; }
+
+    public virtual int CategoryID { get; set; }
+    [ForeignKey("CategoryID")]
+    public virtual FilterCategory Category { get; set; }
+
+    public Object Value { get; set; }
+
+  }
+
+}
